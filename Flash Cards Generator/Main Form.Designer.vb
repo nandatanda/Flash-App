0bfa3fee4490ef5255ae30813caf576b59208b2a
nandatanda <39239764+nandatanda@users.noreply.github.com>
2019-03-17 19:05:04 -0400

    Upload Files
    Upload project files

---
+++ Flash Cards Generator/Main Form.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lstCardTitles = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnNew = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblCaption = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Location = New System.Drawing.Point(242, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(2, 393)
+        Me.Label1.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label3.Location = New System.Drawing.Point(541, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(2, 393)
+        Me.Label3.TabIndex = 4
+        '
+        'lstCardTitles
+        '
+        Me.lstCardTitles.FormattingEnabled = True
+        Me.lstCardTitles.ItemHeight = 20
+        Me.lstCardTitles.Location = New System.Drawing.Point(7, 89)
+        Me.lstCardTitles.Name = "lstCardTitles"
+        Me.lstCardTitles.Size = New System.Drawing.Size(227, 244)
+        Me.lstCardTitles.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 11.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(71, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(108, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "My Flashcards"
+        '
+        'btnNew
+        '
+        Me.btnNew.Location = New System.Drawing.Point(21, 357)
+        Me.btnNew.Name = "btnNew"
+        Me.btnNew.Size = New System.Drawing.Size(75, 30)
+        Me.btnNew.TabIndex = 7
+        Me.btnNew.Text = "New"
+        Me.btnNew.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(145, 357)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 30)
+        Me.btnDelete.TabIndex = 8
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'lblTitle
+        '
+        Me.lblTitle.BackColor = System.Drawing.SystemColors.Window
+        Me.lblTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTitle.Location = New System.Drawing.Point(252, 89)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(281, 35)
+        Me.lblTitle.TabIndex = 9
+        Me.lblTitle.Text = "Title"
+        Me.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblCaption
+        '
+        Me.lblCaption.BackColor = System.Drawing.SystemColors.Window
+        Me.lblCaption.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblCaption.Location = New System.Drawing.Point(252, 139)
+        Me.lblCaption.Name = "lblCaption"
+        Me.lblCaption.Size = New System.Drawing.Size(281, 194)
+        Me.lblCaption.TabIndex = 10
+        Me.lblCaption.Text = "Caption goes here."
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 11.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(348, 30)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(93, 20)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Card Viewer"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 411)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lblCaption)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnNew)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lstCardTitles)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "frmMain"
+        Me.Text = "FlashApp"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lstCardTitles As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnNew As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblCaption As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Flash Cards Generator/Main Form.vb
@@ -0,0 +1,109 @@
+﻿' Name:         Flashcards Project
+' Purpose:      Creates, stores, and retrieves digital flashcards.
+' Programmer:   Ali Elayni on 3.17.2019
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+
+    Private BasePath As String = My.Application.Info.DirectoryPath
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        PopulateMyFlashcards()
+    End Sub
+
+    Private Sub lstCardTitles_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstCardTitles.SelectedIndexChanged
+        PopulateCardViewer()
+    End Sub
+
+    Private Sub btnNew_Click(sender As Object, e As EventArgs) Handles btnNew.Click
+        Dim Title As String = InputBox("Enter a title for your flashcard.", "Create Card", "Title")
+        Dim Caption As String = InputBox("Enter the text you want to appear in the body of the flashcard.", "Create Card", "Text")
+        WriteRecord(Title, Caption)
+
+        PopulateMyFlashcards()
+    End Sub
+
+
+
+
+
+
+    Private Function ReadRecord(ByVal Title As String) As List(Of String)
+        'read a record from data file
+        Dim FilePath As String = IO.Path.Combine(BasePath, "data.txt")
+
+        For Each Line As String In IO.File.ReadLines(FilePath)
+            If Line.Contains(Title) Then
+                Dim Record As New List(Of String)(Line.Split(";"c))
+                Return Record
+                Exit For
+            End If
+        Next
+        Return Nothing
+    End Function
+
+    Private Sub WriteRecord(ByVal Title As String, Caption As String)
+        ' write a record to data file
+        Dim FilePath As String = IO.Path.Combine(BasePath, "data.txt")
+        Dim Record As String = Title + ";" + Caption
+
+        Try
+            Dim File As IO.StreamWriter
+            File = My.Computer.FileSystem.OpenTextFileWriter(FilePath, True)
+            File.WriteLine(Record)
+            File.Close()
+            MessageBox.Show("Your flashcard was stored successfully!", "Success")
+        Catch
+            MessageBox.Show("File access denied", "Error")
+        End Try
+    End Sub
+
+    Private Function ReadRecordTitles() As List(Of String)
+        'return a list of all titles in data file
+        Dim FilePath As String = IO.Path.Combine(BasePath, "data.txt")
+        Dim Titles As New List(Of String)
+
+        For Each Line As String In IO.File.ReadLines(FilePath)
+            Line = Split(Line, ";")(0)
+            Titles.Add(Line)
+        Next
+        Return Titles
+    End Function
+
+    Private Sub PopulateMyFlashcards()
+        ' copy all titles to the listbox
+        lstCardTitles.Items.Clear()
+
+        Dim Title As String
+        For Each Title In ReadRecordTitles()
+            lstCardTitles.Items.Add(Title)
+        Next
+    End Sub
+
+    Private Sub PopulateCardViewer()
+        ' display title and caption of selected item
+        Dim Record As List(Of String) = ReadRecord(lstCardTitles.Text)
+        lblTitle.Text = Record(0)
+        lblCaption.Text = Record(1)
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim LineIndex As Integer
+        Dim FilePath As String = IO.Path.Combine(BasePath, "data.txt")
+        Dim Lines As List(Of String) = System.IO.File.ReadAllLines(FilePath).ToList
+
+        For Each Line As String In Lines
+            If Line.Contains(lstCardTitles.Text) Then
+                Lines.RemoveAt(LineIndex)
+            End If
+            LineIndex += 1
+        Next
+        System.IO.File.WriteAllLines(FilePath, Lines)
+
+        PopulateMyFlashcards()
+    End Sub
+End Class
+
