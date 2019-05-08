d325e87da6bb30624e2fa4a45f1f96ed0e96fb8e
nandatanda <39239764+nandatanda@users.noreply.github.com>
2019-05-08 11:45:34 -0400

    Add Captions to Print Template

--- Flash Cards Generator/Print Template.Designer.vb
+++ Flash Cards Generator/Print Template.Designer.vb
@@ -22,40 +22,38 @@ Partial Class Print_Template
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.PictureBox3 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
         Me.PictureBox5 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
         Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
         Me.PictureBox8 = New System.Windows.Forms.PictureBox()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.lblCaption1 = New System.Windows.Forms.Label()
+        Me.lblCaption3 = New System.Windows.Forms.Label()
+        Me.lblCaption2 = New System.Windows.Forms.Label()
+        Me.lblCaption4 = New System.Windows.Forms.Label()
+        Me.lblCaption6 = New System.Windows.Forms.Label()
+        Me.lblCaption5 = New System.Windows.Forms.Label()
+        Me.lblCaption8 = New System.Windows.Forms.Label()
+        Me.lblCaption7 = New System.Windows.Forms.Label()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.Image = Global.Flash_Cards_Generator.My.Resources.Resources.flashcard_background_blue
-        Me.PictureBox1.Location = New System.Drawing.Point(20, 30)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(173, 95)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox1.TabIndex = 0
-        Me.PictureBox1.TabStop = False
         '
         'PictureBox2
         '
         Me.PictureBox2.Image = Global.Flash_Cards_Generator.My.Resources.Resources.flashcard_background_blue
-        Me.PictureBox2.Location = New System.Drawing.Point(216, 30)
+        Me.PictureBox2.Location = New System.Drawing.Point(309, 16)
         Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(173, 95)
+        Me.PictureBox2.Size = New System.Drawing.Size(272, 170)
         Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox2.TabIndex = 1
         Me.PictureBox2.TabStop = False
@@ -63,96 +61,193 @@ Partial Class Print_Template
         'PictureBox3
         '
         Me.PictureBox3.Image = Global.Flash_Cards_Generator.My.Resources.Resources.flashcard_background_blue
-        Me.PictureBox3.Location = New System.Drawing.Point(216, 144)
+        Me.PictureBox3.Location = New System.Drawing.Point(309, 198)
         Me.PictureBox3.Name = "PictureBox3"
-        Me.PictureBox3.Size = New System.Drawing.Size(173, 95)
+        Me.PictureBox3.Size = New System.Drawing.Size(272, 170)
         Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox3.TabIndex = 3
         Me.PictureBox3.TabStop = False
         '
-        'PictureBox4
-        '
-        Me.PictureBox4.Image = Global.Flash_Cards_Generator.My.Resources.Resources.flashcard_background_blue
-        Me.PictureBox4.Location = New System.Drawing.Point(20, 144)
-        Me.PictureBox4.Name = "PictureBox4"
-        Me.PictureBox4.Size = New System.Drawing.Size(173, 95)
-        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox4.TabIndex = 2
-        Me.PictureBox4.TabStop = False
-        '
         'PictureBox5
         '
         Me.PictureBox5.Image = Global.Flash_Cards_Generator.My.Resources.Resources.flashcard_background_blue
-        Me.PictureBox5.Location = New System.Drawing.Point(216, 258)
+        Me.PictureBox5.Location = New System.Drawing.Point(309, 380)
         Me.PictureBox5.Name = "PictureBox5"
-        Me.PictureBox5.Size = New System.Drawing.Size(173, 95)
+        Me.PictureBox5.Size = New System.Drawing.Size(272, 170)
         Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox5.TabIndex = 5
         Me.PictureBox5.TabStop = False
         '
-        'PictureBox6
-        '
-        Me.PictureBox6.Image = Global.Flash_Cards_Generator.My.Resources.Resources.flashcard_background_blue
-        Me.PictureBox6.Location = New System.Drawing.Point(20, 258)
-        Me.PictureBox6.Name = "PictureBox6"
-        Me.PictureBox6.Size = New System.Drawing.Size(173, 95)
-        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox6.TabIndex = 4
-        Me.PictureBox6.TabStop = False
-        '
         'PictureBox7
         '
         Me.PictureBox7.Image = Global.Flash_Cards_Generator.My.Resources.Resources.flashcard_background_blue
-        Me.PictureBox7.Location = New System.Drawing.Point(216, 372)
+        Me.PictureBox7.Location = New System.Drawing.Point(309, 562)
         Me.PictureBox7.Name = "PictureBox7"
-        Me.PictureBox7.Size = New System.Drawing.Size(173, 95)
+        Me.PictureBox7.Size = New System.Drawing.Size(272, 170)
         Me.PictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox7.TabIndex = 7
         Me.PictureBox7.TabStop = False
         '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Flash_Cards_Generator.My.Resources.Resources.flashcard_background_blue
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 562)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(272, 170)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 11
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.Flash_Cards_Generator.My.Resources.Resources.flashcard_background_blue
+        Me.PictureBox4.Location = New System.Drawing.Point(12, 380)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(272, 170)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 10
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = Global.Flash_Cards_Generator.My.Resources.Resources.flashcard_background_blue
+        Me.PictureBox6.Location = New System.Drawing.Point(12, 198)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(272, 170)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6.TabIndex = 9
+        Me.PictureBox6.TabStop = False
+        '
         'PictureBox8
         '
         Me.PictureBox8.Image = Global.Flash_Cards_Generator.My.Resources.Resources.flashcard_background_blue
-        Me.PictureBox8.Location = New System.Drawing.Point(20, 372)
+        Me.PictureBox8.Location = New System.Drawing.Point(12, 16)
         Me.PictureBox8.Name = "PictureBox8"
-        Me.PictureBox8.Size = New System.Drawing.Size(173, 95)
+        Me.PictureBox8.Size = New System.Drawing.Size(272, 170)
         Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox8.TabIndex = 6
+        Me.PictureBox8.TabIndex = 8
         Me.PictureBox8.TabStop = False
+        '
+        'lblCaption1
+        '
+        Me.lblCaption1.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.lblCaption1.Location = New System.Drawing.Point(33, 33)
+        Me.lblCaption1.Name = "lblCaption1"
+        Me.lblCaption1.Size = New System.Drawing.Size(230, 136)
+        Me.lblCaption1.TabIndex = 12
+        Me.lblCaption1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblCaption3
+        '
+        Me.lblCaption3.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.lblCaption3.Location = New System.Drawing.Point(33, 215)
+        Me.lblCaption3.Name = "lblCaption3"
+        Me.lblCaption3.Size = New System.Drawing.Size(230, 136)
+        Me.lblCaption3.TabIndex = 13
+        Me.lblCaption3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblCaption2
+        '
+        Me.lblCaption2.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.lblCaption2.Location = New System.Drawing.Point(330, 33)
+        Me.lblCaption2.Name = "lblCaption2"
+        Me.lblCaption2.Size = New System.Drawing.Size(230, 136)
+        Me.lblCaption2.TabIndex = 14
+        Me.lblCaption2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblCaption4
+        '
+        Me.lblCaption4.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.lblCaption4.Location = New System.Drawing.Point(330, 215)
+        Me.lblCaption4.Name = "lblCaption4"
+        Me.lblCaption4.Size = New System.Drawing.Size(230, 136)
+        Me.lblCaption4.TabIndex = 15
+        Me.lblCaption4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblCaption6
+        '
+        Me.lblCaption6.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.lblCaption6.Location = New System.Drawing.Point(331, 397)
+        Me.lblCaption6.Name = "lblCaption6"
+        Me.lblCaption6.Size = New System.Drawing.Size(230, 136)
+        Me.lblCaption6.TabIndex = 16
+        Me.lblCaption6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblCaption5
+        '
+        Me.lblCaption5.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.lblCaption5.Location = New System.Drawing.Point(33, 397)
+        Me.lblCaption5.Name = "lblCaption5"
+        Me.lblCaption5.Size = New System.Drawing.Size(230, 136)
+        Me.lblCaption5.TabIndex = 17
+        Me.lblCaption5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblCaption8
+        '
+        Me.lblCaption8.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.lblCaption8.Location = New System.Drawing.Point(330, 579)
+        Me.lblCaption8.Name = "lblCaption8"
+        Me.lblCaption8.Size = New System.Drawing.Size(230, 136)
+        Me.lblCaption8.TabIndex = 18
+        Me.lblCaption8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblCaption7
+        '
+        Me.lblCaption7.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.lblCaption7.Location = New System.Drawing.Point(33, 579)
+        Me.lblCaption7.Name = "lblCaption7"
+        Me.lblCaption7.Size = New System.Drawing.Size(230, 136)
+        Me.lblCaption7.TabIndex = 19
+        Me.lblCaption7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Print_Template
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(409, 511)
-        Me.Controls.Add(Me.PictureBox7)
-        Me.Controls.Add(Me.PictureBox8)
-        Me.Controls.Add(Me.PictureBox5)
-        Me.Controls.Add(Me.PictureBox6)
-        Me.Controls.Add(Me.PictureBox3)
-        Me.Controls.Add(Me.PictureBox4)
-        Me.Controls.Add(Me.PictureBox2)
+        Me.ClientSize = New System.Drawing.Size(593, 749)
+        Me.Controls.Add(Me.lblCaption7)
+        Me.Controls.Add(Me.lblCaption8)
+        Me.Controls.Add(Me.lblCaption5)
+        Me.Controls.Add(Me.lblCaption6)
+        Me.Controls.Add(Me.lblCaption4)
+        Me.Controls.Add(Me.lblCaption2)
+        Me.Controls.Add(Me.lblCaption3)
+        Me.Controls.Add(Me.lblCaption1)
         Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
         Me.Name = "Print_Template"
         Me.Text = "Template"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents PictureBox2 As PictureBox
     Friend WithEvents PictureBox3 As PictureBox
-    Friend WithEvents PictureBox4 As PictureBox
     Friend WithEvents PictureBox5 As PictureBox
-    Friend WithEvents PictureBox6 As PictureBox
     Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
     Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents lblCaption1 As Label
+    Friend WithEvents lblCaption3 As Label
+    Friend WithEvents lblCaption2 As Label
+    Friend WithEvents lblCaption4 As Label
+    Friend WithEvents lblCaption6 As Label
+    Friend WithEvents lblCaption5 As Label
+    Friend WithEvents lblCaption8 As Label
+    Friend WithEvents lblCaption7 As Label
 End Class
